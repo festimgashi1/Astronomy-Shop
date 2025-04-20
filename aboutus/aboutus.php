7a65afd1e889a4bb023f4c422eb408e6768e1d59
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-20 17:23:10 +0200

    update

--- aboutus/aboutus.php
+++ aboutus/aboutus.php
@@ -185,13 +185,13 @@ $page_description = $page_descriptions[$page_title] ?? "";
             </div>
             <nav class="main-nav">
                 <ul>
-                    <li><a href="/Home/index.html" >Home</a></li>
-                    <li><a href="/events/events.html" >Events</a></li>
-                    <li><a href="/news/news.html" >News</a></li>
-                    <li><a href="/Shop/shop.html" >Shop</a></li>
-                    <li><a href="/aboutus/aboutus.html" >About Us</a></li>
-                    <li><a href="/game/game.html">Play</a></li>
-                    <li><a href="/login/login.html"><img src="/WEB2_2025_GR12/login/login.png" style="width: 35px; height: 35px;"></a></li>
+                    <li><a href='/WEB2_2025_GR12/Home/index.php' >Home</a></li>
+                    <li><a href="/WEB2_2025_GR12/events/events.php" >Events</a></li>
+                    <li><a href="/WEB2_2025_GR12/news/news.html" >News</a></li>
+                    <li><a href="/WEB2_2025_GR12/Shop/shop.html" >Shop</a></li>
+                    <li><a href="/WEB2_2025_GR12/aboutus/aboutus.html" >About Us</a></li>
+                    <li><a href="/WEB2_2025_GR12/game/game.php">Play</a></li>
+                    <li><a href="/login/login.php"><img src="/WEB2_2025_GR12/login/login.png" style="width: 35px; height: 35px;"></a></li>
                 </ul>
             </nav>
         </div>
