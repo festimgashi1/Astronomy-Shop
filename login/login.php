f733768a4d4e8053dd9fc048a75a3a69eff0ada2
jonjashari <jonjashari32@gmail.com>
2025-04-20 23:39:52 +0200

    nav update

--- login/login.php
+++ login/login.php
@@ -116,9 +116,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <ul>
                 <li><a href='/WEB2_2025_GR12/Home/index.php' >Home</a></li>
                     <li><a href="/WEB2_2025_GR12/events/events.php" >Events</a></li>
-                    <li><a href="/WEB2_2025_GR12/news/news.html" >News</a></li>
+                    <li><a href="/WEB2_2025_GR12/news/news.php" >News</a></li>
                     <li><a href="/WEB2_2025_GR12/Shop/shop.html" >Shop</a></li>
-                    <li><a href="/WEB2_2025_GR12/aboutus/aboutus.html" >About Us</a></li>
+                    <li><a href="/WEB2_2025_GR12/aboutus/aboutus.php" >About Us</a></li>
                     <li><a href="/WEB2_2025_GR12/game/game.php">Play</a></li>
                     <li><a href="/login/login.php"><img src="/WEB2_2025_GR12/login/login.png" style="width: 35px; height: 35px;"></a></li>
                 </ul>
