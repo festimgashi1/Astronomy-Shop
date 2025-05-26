209c40f34c967021083958e20b77f69659016a69
Getoar19 <getoar.hoxha1@student.uni-pr.edu>
2025-05-26 22:22:57 +0200

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,24 +1,13 @@
- <?php
-  
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "universe";
+<?php
 
-    $con = mysqli_connect($servername , $username, $password , $dbname);
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "universe"; 
 
-    if(!$con){
-        echo "Db connection error..." . mysqli_connect_error();
-    }
-    // else {
-    //     echo "Db connected...!";
-    // }
+$con = mysqli_connect($servername, $username, $password, $dbname, 3307);
 
-
-
-
-
-
-
-          
+if (!$con) {
+    echo "Db connection error: " . mysqli_connect_error();
+}
 ?>
